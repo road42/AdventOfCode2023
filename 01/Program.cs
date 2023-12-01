46229328188b600a7290f91bb33f7cad85b9f5e6
Christoph Jahn <cj@road42.de>
2023-12-01 11:48:05 +0000

    Day 01 Part 1

---
+++ 01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+var lines = File.ReadAllLines("input.txt");
+var sum = 0;
+
+foreach (var line in lines)
+{
+    // find the first and last digit in the line
+    var firstDigit = line.FirstOrDefault(char.IsDigit);
+    var lastDigit = line.LastOrDefault(char.IsDigit);
+
+    // combine the two digits into a number
+    var number = int.Parse(firstDigit.ToString() + lastDigit.ToString(), CultureInfo.InvariantCulture);
+    sum += number;
+}
+
+Console.WriteLine(sum);
+
