cb75568a91fdbde035e79c01749bb1a2a3df6303
Christoph Jahn <cj@road42.de>
2023-12-09 17:50:04 +0100

    D08 P2

--- 08/Program.cs
+++ 08/Program.cs
@@ -18,14 +18,13 @@ for (var i = 1; i < lines.Length; i++)
     nodes[node] = (connections[0], connections[1]);
 }
 
-// Start navigating the nodes
+//Start navigating the nodes
 var currentNode = "AAA";
 var stepCount = 0;
 var instructionPointer = 0;
 
 while (currentNode != "ZZZ")
 {
-    Console.WriteLine($"{currentNode} {stepCount}");
     var direction = instructions[instructionPointer % instructions.Length];
     currentNode = direction == 'L' ? nodes[currentNode].Left : nodes[currentNode].Right;
     instructionPointer++;
@@ -33,3 +32,37 @@ while (currentNode != "ZZZ")
 }
 
 Console.WriteLine($"Result: {stepCount}");
+
+
+// Ghost trip
+// Find all starting nodes (nodes ending with 'A')
+var currentNodes = nodes.Keys.Where(n => n.EndsWith("A", StringComparison.CurrentCulture)).ToArray();
+var steps = 0;
+var startNodes = currentNodes.Length;
+var nodesWithZ = 0;
+var nextNodes = new List<string>();
+
+do
+{
+    steps++;
+    nextNodes.Clear();
+
+    foreach (var node in currentNodes)
+    {
+        var direction = instructions[(steps - 1) % instructions.Length];
+        var nextNode = direction == 'L' ? nodes[node].Left : nodes[node].Right;
+        nextNodes.Add(nextNode);
+    }
+
+    nodesWithZ = nextNodes.Count(n => n.EndsWith("Z", StringComparison.CurrentCulture));
+    nextNodes.CopyTo(currentNodes);
+
+    if (steps % 100000 == 0)
+    {
+        Console.WriteLine($"Step {steps}: {currentNodes.Length} nodes {nodesWithZ} with Z");
+    }
+
+}
+while (nodesWithZ != startNodes);
+
+Console.WriteLine($"All nodes end with Z, stopping at step {steps}");
